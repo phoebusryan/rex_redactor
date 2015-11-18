707efdcb97bc4bced529189df00df485ba318edc
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-18 10:03:17 +0100

    Added 'custom_css' and 'custom_js'

--- install.php
+++ install.php
@@ -1,3 +1,5 @@
 <?php
 	$this->setConfig('redactor_enhancements', 'plugins: [\'rex_mediapool_image\', \'rex_mediapool_link\', \'rex_linkmap\'],');
+	$this->setConfig('custom_css', '');
+	$this->setConfig('custom_js', '');
 ?>
