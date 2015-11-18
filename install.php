4ce984d49ef64c89e7b2ce264f7892dea7951f6a
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-18 14:07:29 +0100

    update for new plugins

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <?php
 	if (!$this->hasConfig()) {
-		$this->setConfig('redactor_enhancements', "/*limiter: 20,\ncounterCallback: function (data) {\n  console.log(data);\n},*/");
+		$this->setConfig('redactor_enhancements', "/*limiter: 20,\ncounterCallback: function (data) {\n  console.log(data);\n},\ntextexpander: [\n  ['lorem', 'Lorem ipsum...'],\n  ['duis', 'Duis autem...']\n],*/");
 		$this->setConfig('custom_css', '');
 		$this->setConfig('custom_js', '');
 	}
