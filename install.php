7502416b9890159bcda990a9474fff49440a828c
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-18 13:30:55 +0100

    Update for new pluginconfiguration

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <?php
 	if (!$this->hasConfig()) {
-		$this->setConfig('redactor_enhancements', 'plugins: [\'rex_mediapool_image\', \'rex_mediapool_link\', \'rex_linkmap\'],');
+		$this->setConfig('redactor_enhancements', "/*limiter: 20,\ncounterCallback: function (data) {\n  console.log(data);\n},*/");
 		$this->setConfig('custom_css', '');
 		$this->setConfig('custom_js', '');
 	}
