68fd2d6ccf0570f18541d6cbf8b08818fcfb6039
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-20 16:01:02 +0100

    beispiel angepasst

--- pages/tutorial.php
+++ pages/tutorial.php
@@ -5,11 +5,7 @@
 	$moduleInput .= '<fieldset>'.PHP_EOL;
 	$moduleInput .= '  <dl class="rex-form-group form-group">'.PHP_EOL;
 	$moduleInput .= '    <dt><label for="redactor_1">VALUE 1:</label></dt>'.PHP_EOL;
-	$moduleInput .= '    <dd><textarea cols="1" rows="6" class="form-control redactorEditor-default" id="redactor_1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea></dd>'.PHP_EOL;
-	$moduleInput .= '  </dl>'.PHP_EOL;
-	$moduleInput .= '  <dl class="rex-form-group form-group">'.PHP_EOL;
-	$moduleInput .= '    <dt><label for="redactor_2">VALUE 2:</label></dt>'.PHP_EOL;
-	$moduleInput .= '    <dd><textarea cols="1" rows="6" class="form-control redactorEditor-simple" id="redactor_2" name="REX_INPUT_VALUE[2]">REX_VALUE[2]</textarea></dd>'.PHP_EOL;
+	$moduleInput .= '    <dd><textarea cols="1" rows="6" class="form-control redactorEditor-full" id="redactor_1" name="REX_INPUT_VALUE[1]">REX_VALUE[1]</textarea></dd>'.PHP_EOL;
 	$moduleInput .= '  </dl>'.PHP_EOL;
 	$moduleInput .= '</fieldset>'.PHP_EOL;
 	
