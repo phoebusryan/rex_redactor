bd6988d969a062f0812e1e1cb2ff698e4fca96e2
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-20 16:02:15 +0100

    setup geupdatet

--- install.sql
+++ install.sql
@@ -5,9 +5,8 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%redactor_profiles` (
   `name` varchar(30) NOT NULL DEFAULT '',
   `description` varchar(255) NOT NULL DEFAULT '',
   `language` varchar(10) NOT NULL DEFAULT '',
-  `redactor_configuration` text NOT NULL
+  `redactor_buttons` text NOT NULL
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
-INSERT INTO `%TABLE_PREFIX%redactor_profiles` (`id`, `name`, `description`, `language`, `redactor_configuration`) VALUES
-(2, 'simple', 'Standard Redactor-Konfiguration', 'en', 'buttons: [\r\n	''html'', ''|'',\r\n	''formatting'', ''|'',\r\n	''bold'', ''|'',\r\n	''unorderedlist'', ''|'',\r\n	],\r\n\r\nformatting: [''p'', ''h3''],'),
-(1, 'default', 'Full', 'de', '');
+INSERT INTO `%TABLE_PREFIX%redactor_profiles` (`id`, `name`, `description`, `language`, `redactor_buttons`) VALUES
+(1, 'full', 'Standard Redactor-Konfiguration', 'de', 'alignment,bold,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,fontcolor[#ffffff|#000000],fontfamily[arial|times],fontsize[10|12|14],formatting[h1|p],fullscreen,italic,orderedlist,rex_linkmap,rex_mediapool_image,rex_mediapool_link,table,textexpander[Lorem=Lorem ipsum|Duis=Duis aute],unorderedlist,underline,video');
