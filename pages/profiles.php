95450d0f499653965748ee040f75dc9956b6d631
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-20 16:01:20 +0100

    buttons + hilfetext hinzugefügt

--- pages/profiles.php
+++ pages/profiles.php
@@ -73,10 +73,83 @@
 			//End - get all languages from the assets-folder
 		//End - add language-field
 		
-		//Start - add redactor_configuration-field
-			$field = $form->addTextAreaField('redactor_configuration');
-			$field->setLabel($this->i18n('profiles_label_redactorconfiguration'));
-		//End - add redactor_configuration-field
+		//Start - add redactor_buttons-field
+			$field = $form->addTextAreaField('redactor_buttons');
+			$field->setLabel($this->i18n('profiles_label_redactorbuttons'));
+			
+			$field = $form->addRawField('
+				<dl class="rex-form-group form-group">
+					<dt>
+						&nbsp;
+					</dt>
+					<dd>
+						<p><a href="javascript:void(0);" onclick="$(\'#rex-redactor-help\').toggle(\'fast\');">Zeige/verberge Hilfe</a></p>
+						<div id="rex-redactor-help" style="display:none">'.
+							'<b>alignment</b><br>'.
+							$this->i18n('profiles_buttons_alignment').'<br>'.
+							'<br>'.
+							'<b>bold</b><br>'.
+							$this->i18n('profiles_buttons_bold').'<br>'.
+							'<br>'.
+							'<b>clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2]</b><br>'.
+							$this->i18n('profiles_buttons_clips').'<br>'.
+							'<br>'.
+							'<b>deleted</b><br>'.
+							$this->i18n('profiles_buttons_deleted').'<br>'.
+							'<br>'.
+							'<b>fontcolor[#ffffff|#000000]</b><br>'.
+							$this->i18n('profiles_buttons_fontcolor').'<br>'.
+							'<br>'.
+							'<b>fontfamily[arial|times]</b><br>'.
+							$this->i18n('profiles_buttons_fontfamily').'<br>'.
+							'<br>'.
+							'<b>fontsize[10|12|14]</b><br>'.
+							$this->i18n('profiles_buttons_fontsize').'<br>'.
+							'<br>'.
+							'<b>formatting[h1|p]</b><br>'.
+							$this->i18n('profiles_buttons_formatting').'<br>'.
+							'<br>'.
+							'<b>fullscreen</b><br>'.
+							$this->i18n('profiles_buttons_fullscreen').'<br>'.
+							'<br>'.
+							'<b>italic</b><br>'.
+							$this->i18n('profiles_buttons_italic').'<br>'.
+							'<br>'.
+							'<b>orderedlist</b><br>'.
+							$this->i18n('profiles_buttons_orderedlist').'<br>'.
+							'<br>'.
+							'<b>rex_linkmap</b><br>'.
+							$this->i18n('profiles_buttons_rex_linkmap').'<br>'.
+							'<br>'.
+							'<b>rex_mediapool_image</b><br>'.
+							$this->i18n('profiles_buttons_rex_mediapool_image').'<br>'.
+							'<br>'.
+							'<b>rex_mediapool_link</b><br>'.
+							$this->i18n('profiles_buttons_rex_mediapool_link').'<br>'.
+							'<br>'.
+							'<b>table</b><br>'.
+							$this->i18n('profiles_buttons_table').'<br>'.
+							'<br>'.
+							'<b>textdirection</b><br>'.
+							$this->i18n('profiles_buttons_textdirection').'<br>'.
+							'<br>'.
+							'<b>textexpander[Lorem=Lorem ipsum|Duis=Duis aute]</b><br>'.
+							$this->i18n('profiles_buttons_textexpander').'<br>'.
+							'<br>'.
+							'<b>underline</b><br>'.
+							$this->i18n('profiles_buttons_underline').'<br>'.
+							'<br>'.
+							'<b>unorderedlist</b><br>'.
+							$this->i18n('profiles_buttons_unorderedlist').'<br>'.
+							'<br>'.
+							'<b>video</b><br>'.
+							$this->i18n('profiles_buttons_video').'<br>'.
+							'
+						</div>
+					</dd>
+				</dl>
+			');
+		//End - add redactor_buttons-field
 		
 		if ($func == 'edit') {
 			$form->addParam('id', $id);
