24d60acbfb2efc663a54c19aecbc3715a6484709
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-11-21 07:52:10 +0100

    Setup an neue Buttons angepasst

--- install.sql
+++ install.sql
@@ -9,4 +9,4 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%redactor_profiles` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
 
 INSERT INTO `%TABLE_PREFIX%redactor_profiles` (`id`, `name`, `description`, `language`, `redactor_buttons`) VALUES
-(1, 'full', 'Standard Redactor-Konfiguration', 'de', 'alignment,bold,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,fontcolor[#ffffff|#000000],fontfamily[arial|times],fontsize[10|12|14],formatting[h1|p],fullscreen,italic,orderedlist,rex_linkmap,rex_mediapool_image,rex_mediapool_link,table,textexpander[Lorem=Lorem ipsum|Duis=Duis aute],unorderedlist,underline,video');
+(1, 'full', 'Standard Redactor-Konfiguration', 'de', 'html,alignment,bold,clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],deleted,fontcolor[#ffffff|#000000],fontfamily[arial|times],fontsize[10|12|14],formatting[h1|p],fullscreen,italic,orderedlist,rex_linkmap,rex_mediapool_image,rex_mediapool_link,table,textexpander[Lorem=Lorem ipsum|Duis=Duis aute],unorderedlist,underline,video,indent,outdent,horizontalrule');
