a6a340fa6cf7038859b2783dd076b1eecebc96b1
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-04 16:53:33 +0100

    Bugfix: AUTO_INCREMENT fehlte

--- install.sql
+++ install.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%redactor_profiles` (
   `language` varchar(10) NOT NULL DEFAULT '',
   `redactor_buttons` text NOT NULL,
   `redactor_plugins` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 INSERT INTO `%TABLE_PREFIX%redactor_profiles` (`id`, `name`, `description`, `language`, `redactor_buttons`, `redactor_plugins`) VALUES
 (1, 'full', 'Standard Redactor-Konfiguration', 'de', 'alignment,bold,deleted,formatting[h1|p],horizontalrule,html,italic,indent,orderedlist,outdent,underline,unorderedlist', 'clips[Snippetname1=Snippettext1|Snippetname2=Snippettext2],fontcolor[#ffffff|#000000],fontfamily[arial|times],fontsize[10|12|14],fullscreen,rex_linkmap,rex_mediapool_image,rex_mediapool_link,table,textdirection,video');
