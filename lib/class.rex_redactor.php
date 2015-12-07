c2fa598e6a054610b3cc0ce38b4e3f3a25eb3e59
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-07 09:02:11 +0100

    Variable und DB-Feld umbenannt

--- lib/class.rex_redactor.php
+++ lib/class.rex_redactor.php
@@ -1,13 +1,13 @@
 <?php
 	class rex_redactor {
 		
-		public static function insertProfile ($name, $description = '', $redactorButtons = '', $redactorProfiles = '') {
+		public static function insertProfile ($name, $description = '', $redactorButtons = '', $redactorPlugins = '') {
 			$sql = rex_sql::factory();
 			$sql->setTable(rex::getTablePrefix().'redactor_profiles');
 			$sql->setValue('name', $name);
 			$sql->setValue('description', $description);
 			$sql->setValue('redactor_buttons', $redactorButtons);
-			$sql->setValue('redactor_profile', $redactorProfiles);
+			$sql->setValue('redactor_plugins', $redactorPlugins);
 			
 			try {
 				$sql->insert();
