9f10ba013ee94fea9ab0e92a816e7b25ccd599dd
Thomas Kaegi <thomas.kaegi@factorylabs.com>
2015-12-11 15:37:14 +0100

    Feld 'language' entfernt

--- update.php
+++ update.php
@@ -1,4 +1,8 @@
 <?php
+	if (rex_string::versionCompare($this->getVersion(), '1.4', '<')) {
+		rex_sql_table::get(rex::getTable('redactor_profiles'))->removeColumn('language')->alter();
+	}
+
 	if (rex_string::versionCompare($this->getVersion(), '1.3', '<')) {
 		rex_sql_table::get(rex::getTable('redactor_profiles'))->ensureColumn(new rex_sql_column('redactor_plugins', 'TEXT'))->alter();
 	}
